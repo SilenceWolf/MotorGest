1e1f868ef4e588d82ae72cf266db72348006e671
isidoro <isidoro.matheo@gmail.com>
2026-05-12 13:03:56 +0200

    DialogVehicule supporte le type Special

--- src/view/DialogVehicule.java
+++ src/view/DialogVehicule.java
@@ -15,12 +15,14 @@ public class DialogVehicule extends JDialog {
     private JTextField marqueField;
     private JTextField modeleField;
     private JTextField kmField;
-    private JTextField extraField;
-    private JLabel extraLabel;
+    private JTextField extra1Field;
+    private JTextField extra2Field;
+    private JLabel extra1Label;
+    private JLabel extra2Label;
 
     public DialogVehicule(JFrame parent, Vehicule existant) {
         super(parent, existant == null ? "Nouveau vehicule" : "Modifier vehicule", true);
-        setSize(440, 380);
+        setSize(440, 420);
         setLocationRelativeTo(parent);
         getContentPane().setBackground(Theme.FOND_CARTE);
         construire(existant);
@@ -41,7 +43,7 @@ public class DialogVehicule extends JDialog {
         gbc.gridx = 0; gbc.gridy = row; gbc.weightx = 0;
         form.add(Theme.etiquette("Type"), gbc);
         gbc.gridx = 1; gbc.weightx = 1;
-        typeCombo = Theme.combo(new String[]{"Leger", "Lourd"});
+        typeCombo = Theme.combo(new String[]{"Leger", "Lourd", "Special"});
         form.add(typeCombo, gbc);
         row++;
 
@@ -74,11 +76,21 @@ public class DialogVehicule extends JDialog {
         row++;
 
         gbc.gridx = 0; gbc.gridy = row; gbc.weightx = 0;
-        extraLabel = Theme.etiquette("Nb places");
-        form.add(extraLabel, gbc);
+        extra1Label = Theme.etiquette("Nb places");
+        form.add(extra1Label, gbc);
         gbc.gridx = 1; gbc.weightx = 1;
-        extraField = Theme.champTexte(18);
-        form.add(extraField, gbc);
+        extra1Field = Theme.champTexte(18);
+        form.add(extra1Field, gbc);
+        row++;
+
+        gbc.gridx = 0; gbc.gridy = row; gbc.weightx = 0;
+        extra2Label = Theme.etiquette("Niveau urgence");
+        form.add(extra2Label, gbc);
+        gbc.gridx = 1; gbc.weightx = 1;
+        extra2Field = Theme.champTexte(18);
+        form.add(extra2Field, gbc);
+        extra2Label.setVisible(false);
+        extra2Field.setVisible(false);
 
         typeCombo.addActionListener(e -> majChampsExtras());
 
@@ -90,10 +102,14 @@ public class DialogVehicule extends JDialog {
             kmField.setText(String.valueOf(existant.getKilometrage()));
             if (existant instanceof VehiculeLeger) {
                 typeCombo.setSelectedItem("Leger");
-                extraField.setText(String.valueOf(((VehiculeLeger) existant).getNbPlaces()));
+                extra1Field.setText(String.valueOf(((VehiculeLeger) existant).getNbPlaces()));
             } else if (existant instanceof VehiculeLourd) {
                 typeCombo.setSelectedItem("Lourd");
-                extraField.setText(String.valueOf(((VehiculeLourd) existant).getChargeMaxTonnes()));
+                extra1Field.setText(String.valueOf(((VehiculeLourd) existant).getChargeMaxTonnes()));
+            } else if (existant instanceof VehiculeSpecial) {
+                typeCombo.setSelectedItem("Special");
+                extra1Field.setText(((VehiculeSpecial) existant).getTypeSpecial());
+                extra2Field.setText(String.valueOf(((VehiculeSpecial) existant).getNiveauUrgence()));
             }
             typeCombo.setEnabled(false);
             majChampsExtras();
@@ -118,8 +134,8 @@ public class DialogVehicule extends JDialog {
         JPanel bas = new JPanel(new FlowLayout(FlowLayout.RIGHT, 8, 12));
         bas.setBackground(Theme.FOND);
         bas.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Theme.BORDURE));
-        JButton ok      = Theme.boutonSucces("Valider");
-        JButton annuler = Theme.boutonNeutre("Annuler");
+        JButton ok      = Theme.boutonSucces("Valider", "[v]");
+        JButton annuler = Theme.boutonNeutre("Annuler", "[x]");
         bas.add(annuler);
         bas.add(ok);
         ok.addActionListener(e -> valider());
@@ -129,8 +145,20 @@ public class DialogVehicule extends JDialog {
 
     private void majChampsExtras() {
         String type = (String) typeCombo.getSelectedItem();
-        if ("Leger".equals(type)) extraLabel.setText("Nb places");
-        else extraLabel.setText("Charge (t)");
+        if ("Leger".equals(type)) {
+            extra1Label.setText("Nb places");
+            extra2Label.setVisible(false);
+            extra2Field.setVisible(false);
+        } else if ("Lourd".equals(type)) {
+            extra1Label.setText("Charge (t)");
+            extra2Label.setVisible(false);
+            extra2Field.setVisible(false);
+        } else {
+            extra1Label.setText("Type special");
+            extra2Label.setText("Niveau urgence");
+            extra2Label.setVisible(true);
+            extra2Field.setVisible(true);
+        }
         revalidate();
         repaint();
     }
@@ -153,11 +181,15 @@ public class DialogVehicule extends JDialog {
 
             String type = (String) typeCombo.getSelectedItem();
             if ("Leger".equals(type)) {
-                int places = Integer.parseInt(extraField.getText().trim());
+                int places = Integer.parseInt(extra1Field.getText().trim());
                 resultat = new VehiculeLeger(immat, marque, modele, km, places);
-            } else {
-                double charge = Double.parseDouble(extraField.getText().trim());
+            } else if ("Lourd".equals(type)) {
+                double charge = Double.parseDouble(extra1Field.getText().trim());
                 resultat = new VehiculeLourd(immat, marque, modele, km, charge);
+            } else {
+                String typeSpe = extra1Field.getText().trim();
+                int niv = Integer.parseInt(extra2Field.getText().trim());
+                resultat = new VehiculeSpecial(immat, marque, modele, km, typeSpe, niv);
             }
             dispose();
         } catch (NumberFormatException ex) {
